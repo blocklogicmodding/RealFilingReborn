0bb1d3c5a62e8a0117821cdbd14b5bab48466c04
deonjonker123 <deon.jonker@hotmail.com>
2025-04-22 17:08:31 +0200

    Re-textured all blocks and items

--- src/main/java/com/blocklogic/realfilingreborn/block/custom/FilingIndexBlock.java
+++ src/main/java/com/blocklogic/realfilingreborn/block/custom/FilingIndexBlock.java
@@ -3,18 +3,26 @@ package com.blocklogic.realfilingreborn.block.custom;
 import com.blocklogic.realfilingreborn.block.entity.FilingIndexBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class FilingIndexBlock extends BaseEntityBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final MapCodec<FilingIndexBlock> CODEC = simpleCodec(FilingIndexBlock::new);
 
     public FilingIndexBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
@@ -25,6 +33,16 @@ public class FilingIndexBlock extends BaseEntityBlock {
     @Override
     public RenderShape getRenderShape(BlockState state) {
         return RenderShape.MODEL;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Nullable
