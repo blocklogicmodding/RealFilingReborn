476f155cfa6b9d7be6ca15accc5419aa82b748b2
blocklogicmodding <deon.jonker1990@hotmail.com>
2025-06-23 21:38:06 +0200

    1.0.6: REMOVED ALL NETWORKING BULLSHIT!

--- src/main/java/com/blocklogic/realfilingreborn/util/RFRTags.java
+++ src/main/java/com/blocklogic/realfilingreborn/util/RFRTags.java
@@ -11,8 +11,6 @@ import net.minecraft.world.level.block.Block;
 public class RFRTags {
     public static class Blocks {
         public static final TagKey<Block> CABINETS = createTag("cabinets");
-        public static final TagKey<Block> INDEX_CABLES = createTag("index_cables");
-        public static final TagKey<Block> FILING_CONTROLLERS = createTag("filing_controllers");
 
         private static TagKey<Block> createTag (String name) {
             return BlockTags.create(ResourceLocation.fromNamespaceAndPath(RealFilingReborn.MODID, name));
@@ -22,7 +20,6 @@ public class RFRTags {
     public static class Items {
         public static final TagKey<Item> FOLDERS = createTag("folders");
         public static final TagKey<Item> ARCHIVE_TOOLS = createTag("archive_tools");
-        public static final TagKey<Item> CABINETS = createTag("cabinets");
 
         private static TagKey<Item> createTag (String name) {
             return ItemTags.create(ResourceLocation.fromNamespaceAndPath(RealFilingReborn.MODID, name));
